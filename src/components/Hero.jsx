f567c6d8b20071636cd3f86d7dbd3a9f7a013c5b
Varun Devgan <varun.devgan@outlook.com>
2025-06-10 07:32:29 +0530

    removed loader

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -84,16 +84,16 @@ const Hero = () => {
 
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
-      {loading && (
+      {/* https://uiverse.io/G4b413l/tidy-walrus-92 */}
+      {/* {loading && (
         <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
-          {/* https://uiverse.io/G4b413l/tidy-walrus-92 */}
           <div className="three-body">
             <div className="three-body__dot"></div>
             <div className="three-body__dot"></div>
             <div className="three-body__dot"></div>
           </div>
         </div>
-      )}
+      )} */}
 
       <div
         id="video-frame"
